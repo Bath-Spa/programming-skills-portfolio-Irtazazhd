f3f4858475da0020f1efed85c77a6420a406a9cf
Irtazazhd <muhammadirtaza560@gmail.com>
2023-11-05 02:01:44 +0400

    ch3, ex2

---
+++ Chapter 3- Data Structures/Exercises/Exercise 2.py
@@ -0,0 +1,6 @@
+# Create a list of your friends' names
+names = ["Faseeh", "Irti", "Abdu", "Taha", "Topper"]
+
+# Print a personalized message to each person
+for name in names:
+    print(f"Hello, {name}! Have a great day.")
