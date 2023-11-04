51a54eb6c7157a54e08253be35436a2b572b137f
Irtazazhd <muhammadirtaza560@gmail.com>
2023-11-05 00:05:09 +0400

    ch2, ex2

---
+++ Chapter 2- Variables & Comments/exercise 2.py
@@ -0,0 +1,6 @@
+# Define the quote and its author
+quote = "You must be the change you wish to see in the world."
+author = "Muhammad Faseeh"
+
+# Print the quote and its author
+print(f'{author} once said, "{quote}"')
---
+++ Chapter 2- Variables & Comments/exercise 4.py
@@ -0,0 +1,8 @@
+# Define your favorite number
+favorite_number = 5
+
+# Create a message to reveal your favorite number
+message = f"My favorite number is {favorite_number}."
+
+# Print the message
+print(message)
