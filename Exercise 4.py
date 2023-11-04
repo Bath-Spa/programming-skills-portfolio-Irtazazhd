d0dfd9e09328e747fec30a4cee1de06722cc9495
Irtazazhd <muhammadirtaza560@gmail.com>
2023-11-05 01:00:12 +0400

    ch3, ex4

---
+++ Exercise 4.py
@@ -0,0 +1,6 @@
+# Create a list of people to invite to dinner
+guests = ["Abdu", "Topper", "cafeteria"]
+
+# Generate invitation messages for each guest
+for guest in guests:
+    print(f"Dear {guest}, I would like to invite you to a dinner at my place. It would be an honor to have you join us.")
