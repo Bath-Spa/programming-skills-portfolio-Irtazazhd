3d78f6e89f13dc9644aa76b3956c241eab73517e
Irtazazhd <muhammadirtaza560@gmail.com>
2023-11-05 02:02:01 +0400

    ch3, ex3

---
+++ Chapter 3- Data Structures/Exercises/Exercise 3.py
@@ -0,0 +1,6 @@
+# Create a list of car models
+cars = ["Nissan SKyline", "Toyota Supra", "Mercedes Benz", "Nissan Patrol"]
+
+# Print statements about each car
+for car in cars:
+    print(f"I would like to own a {car}.")
