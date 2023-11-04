13597648b5360b95a41b31419d88911a79ba7ba3
Irtazazhd <muhammadirtaza560@gmail.com>
2023-11-05 00:19:01 +0400

    ch, ex5

--- Chapter 2- Variables & Comments/exercise 5.py
+++ Chapter 2- Variables & Comments/exercise 5.py
@@ -1,2 +1,13 @@
-import turtle
-turtle.circle(100)
+# Define the cost of a single USB stick and the girl's budget
+usb_stick_price = 6
+budget = 50
+
+# Calculate how many USB sticks she can buy
+usb_sticks_purchased = budget // usb_stick_price
+
+# Calculate how much money she will have left
+remaining_money = budget % usb_stick_price
+
+# Print the results
+print(f"She can buy {usb_sticks_purchased} USB sticks.")
+print(f"She will have £{remaining_money} left.")
